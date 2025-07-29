bc14c933cc9bcc7168693cb0a3e9e05d3b6315dd
PhamTan <phamphutanxo@gmail.com>
2025-07-29 10:50:33 +0700

    docs: setup and custom theme

---
+++ pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () => {
+  return (
+    <div className="min-h-screen container">
+      {/* Theme Toggle */}
+
+      {/* Background Effects  */}
+
+      {/* Navbar */}
+
+      {/* Main content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
