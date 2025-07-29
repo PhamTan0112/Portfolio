01de88b4aa95197692c409845a5e204330ae55e5
PhamTan <phamphutanxo@gmail.com>
2025-07-29 16:52:14 +0700

    feat: Navbar

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,16 @@
+import { Navbar } from "../src/conponents/Navbar";
+import { StarBackground } from "../src/conponents/StarBackground";
+import { ThemeToggle } from "../src/conponents/ThemeToggle";
+
 export const Home = () => {
   return (
-    <div className="min-h-screen container">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
-
+      <ThemeToggle></ThemeToggle>
       {/* Background Effects  */}
-
+      <StarBackground></StarBackground>
       {/* Navbar */}
-
+      <Navbar></Navbar>
       {/* Main content */}
 
       {/* Footer */}
